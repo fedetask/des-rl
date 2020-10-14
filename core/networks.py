5eb3b1bb67bf1e2d65ae1049a0371e1965e5abdb
Federico <federico.taschin@studenti.unipd.it>
2020-10-14 20:55:02 +0200

    Fix LinearNetwork activation and add output weight

--- core/networks.py
+++ core/networks.py
@@ -8,28 +8,38 @@ class LinearNetwork(nn.Module):
     """A network composed of several linear layers.
     """
 
-    def __init__(self, inputs, outputs, n_layers, n_units, activation=torch.tanh,
-                 activation_last_layer=False):
+    def __init__(self, inputs, outputs, n_hidden_layers, n_hidden_units, activation=torch.relu,
+                 activation_last_layer=None, output_weight=1., dtype=torch.float):
         """Create a linear neural network with the given number of layers and units and
         the given activations.
 
         Args:
             inputs (int): Number of input nodes.
             outputs (int): Number of output nodes.
-            n_layers (int): Total number of layers, including input and output layers.
-            n_units (int): Number of units in the hidden layers.
-            activation (function): The activation function to use. Use None for no activation.
-            activation_last_layer (bool): Whether to apply the activation function in the
-                output layer.
+            n_hidden_layers (int): Number of hidden layers, excluding input and output layers.
+            n_hidden_units (int): Number of units in the hidden layers.
+            activation: The activation function that will be used in all but the last layer. Use
+                None for no activation.
+            activation_last_layer: The activation function to be used in the last layer. Use None
+                for no activation.
+            output_weight (float): Weight(s) to multiply to the last layer output.
+            dtype (torch.dtype): Type of the network weights.
         """
         super().__init__()
         self.activation = activation
         self.activation_last_layer = activation_last_layer
-        self.lin = nn.Linear(in_features=inputs, out_features=n_units)
-        self.hidden_layers = [
-            nn.Linear(in_features=n_units, out_features=n_units) for i in range(n_layers - 2)
-        ]
-        self.lout = nn.Linear(in_features=n_units, out_features=outputs)
+        self.output_weight = output_weight
+        self.lin = nn.Linear(in_features=inputs, out_features=n_hidden_units)
+        self.hidden_layers = torch.nn.ModuleList()
+        for i in range(n_hidden_layers):
+            self.hidden_layers.append(
+                nn.Linear(
+                    in_features=n_hidden_units,
+                    out_features=n_hidden_units
+                )
+            )
+        self.lout = nn.Linear(in_features=n_hidden_units, out_features=outputs)
+        self.type(dtype)
 
     def forward(self, *inputs):
         """Forward pass on the concatenation of the given inputs.
@@ -43,6 +53,6 @@ class LinearNetwork(nn.Module):
             if self.activation is not None:
                 x = self.activation(x)
         x = self.lout(x)
-        if self.activation is not None and self.activation_last_layer:
-            x = self.activation(x)
+        if self.activation_last_layer is not None:
+            x = self.activation_last_layer(x) * self.output_weight
         return x
