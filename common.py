69b6e4ca0b59432eafcab38c5d57e1837f6b82e4
Federico <federico.taschin@studenti.unipd.it>
2020-10-15 19:41:13 +0200

    Create ParameterUpdater and fix clamp()

--- common.py
+++ common.py
@@ -4,20 +4,123 @@ import torch
 import numpy as np
 
 
+class ParameterUpdater:
+    """ParameterUpdater class that stores a parameter and handles its update according to the
+    selected schedule.
+
+    For 'const' schedule, the parameter is left to its start value.
+    For 'lin' schedule the parameter is updated as param(t) = start - t * (start - end) / n_steps.
+    For 'exp' schedule, the parameter is updated as param(t) = alpha * exp(-beta * t),
+    where alpha and beta are set such that param(0) = start and param(n_steps) = end.
+    """
+
+    VALID_SCHEDULES = ['cons', 'lin', 'exp']
+
+    def __init__(self, start, end, n_steps, update_schedule='lin'):
+        """Create the ParameterUpdater.
+
+        Args:
+            start (float): Initial value of the parameter.
+            end (float): Final value of the parameter.
+            n_steps (int): Total number of steps over which the parameter is updated.
+            update_schedule (str): Either 'const', 'lin', or 'exp'.
+        """
+        assert update_schedule in ParameterUpdater.VALID_SCHEDULES,\
+            'The given schedule is not understood. Use ' + str(ParameterUpdater.VALID_SCHEDULES)
+        self.start = start
+        self.end = end
+        self.n_steps = n_steps
+        self.update_schedule = update_schedule
+        self.cur_value = start
+        self.step = 0
+
+    def update(self):
+        """Perform one update step of the parameter, i.e. updating it from param(t) to param(t+1).
+        """
+        if self.update_schedule == 'const':
+            pass
+        elif self.update_schedule == 'lin':
+            self.cur_value = self.start - self.step * (self.start - self.end) / self.n_steps
+        elif self.update_schedule == 'exp':
+            beta = (-1. / self.n_steps) * np.log(float(self.end / self.start))
+            self.cur_value = self.start * np.exp(-beta * self.step)
+        self.step += 1
+
+    def get_value(self):
+        """Returns: The current value of the parameter.
+        """
+        return self.cur_value
+
+
 def clamp(tensor, min_value, max_value):
-    if isinstance(max_value, numbers.Number):
-        clamped = tensor.clamp(min_value, max_value)
-    elif isinstance(max_value, torch.Tensor):
-        clamped = torch.max(
-            torch.min(tensor, max_value),
-            min_value
-        )
-    elif isinstance(max_value, np.ndarray):
-        clamped = torch.max(
-            torch.min(tensor, torch.from_numpy(max_value)),
-            torch.from_numpy(min_value)
-        )
+    """Clamp the tensor in the given range.
+
+    Args:
+        tensor (torch.Tensor): Tensor whose elements have to be clipped.
+        min_value (Union[float, torch.Tensor, numpy.ndarray]): Low clipping value. If not a
+            scalar, its shape must match tensor and the clipping is be performed element wise.
+        max_value (Union[float, torch.Tensor]): High clipping value. If a Tensor is given,
+            its shape must match tensor and the clipping is be performed element wise.
+
+    Returns:
+        A copy of the given tensor where each element is clipped in the given range.
+    """
+    if min_value is None and max_value is None:
+        return tensor
+    elif max_value is not None and min_value is None:
+        if isinstance(max_value, float):
+            return torch.clamp(tensor, max=max_value)
+        else:
+            return torch.min(tensor, max_value)
+    elif min_value is not None and max_value is None:
+        if isinstance(min_value, float):
+            return torch.clamp(tensor, min=min_value)
+        else:
+            return torch.max(tensor, min_value)
     else:
-        raise TypeError('The given max_action type ' + str(type(max_value)) +
-                        ' is not understood.')
-    return clamped
+        if isinstance(min_value, numbers.Number) and isinstance(max_value, numbers.Number):
+            return torch.clamp(tensor, min=min_value, max=max_value)
+        elif isinstance(min_value, float) and isinstance(max_value, torch.Tensor):
+            return torch.max(torch.min(tensor, max_value), torch.clamp(tensor, min=min_value))
+        elif isinstance(min_value, torch.Tensor) and isinstance(max_value, float):
+            return torch.max(torch.min(tensor, torch.clamp(tensor, max=max_value)), min_value)
+        else:
+            return torch.max(torch.min(tensor, max_value), min_value)
+
+
+def split_replay_batch(samples):
+    """Takes a list of transitions (s, a, r, s') and splits them into arrays.
+
+    Args:
+        samples (list): List of tuples (s, a, r, s'), where:
+            - s: numpy array, all s must have the same shape.
+            - a: numpy array, all a must have the same shape.
+            - r: float.
+            - s': numpy array, all non None s' must have the same shape.
+
+    Returns:
+        A tuple (states, actions, rewards, next_states, next_states_idx) where each element
+        is a numpy array with correspondent data in the rows. Pay attention that the rewards
+        array is a column vector.
+
+        Since the next state can be None when episode ends, the number of elements in next_states
+        can be < len(samples). For this reason, next_states_idx is a numpy array that contains
+        the actual indices of elements in next_states.
+    """
+    states = np.array([transition[0] for transition in samples])
+    actions = np.array([transition[1] for transition in samples])
+    rewards = np.array([transition[2] for transition in samples])
+    if len(rewards.shape) == 1:
+        rewards = np.expand_dims(rewards, 1)
+    if len(actions.shape) == 1:
+        actions = np.expand_dims(actions, 1)
+
+    next_states = []
+    next_states_idx = []
+    for i, transition in enumerate(samples):
+        if transition[3] is not None:
+            next_states.append(transition[3])
+            next_states_idx.append(i)
+    next_states = np.array(next_states)
+    next_states_idx = np.array(next_states_idx)
+    return states, actions, rewards, next_states, next_states_idx
