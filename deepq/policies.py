04aac774c6610dc0c985148a4d241bc6d6af829c
Federico <federico.taschin@studenti.unipd.it>
2020-10-15 19:42:04 +0200

    Use ParameterUpdater in policies

--- deepq/policies.py
+++ deepq/policies.py
@@ -19,8 +19,14 @@ Content:
 """
 
 import abc
+import sys
+import os
+
 import torch
 import numpy as np
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import common
 
 
 class BasePolicy(abc.ABC):
@@ -41,19 +47,22 @@ class BasePolicy(abc.ABC):
 
 
 class EpsilonGreedyPolicy(BasePolicy):
-    """Base class for an epsilon-greedy policy.
-
-    Implementations of this class should use _update_params() to handle parameter updating every
-    time act() is called.
+    """Base class for an epsilon-greedy policy, that selects the argmax action with probability
+    p = 1 - epsilon and a random action with p = epsilon.
     """
 
-    def __init__(self, epsilon):
+    def __init__(self, start_epsilon, end_epsilon, decay_steps, decay_schedule='lin'):
         """Instantiate the base epsilon greedy policy.
 
         Args:
-            epsilon (float): Probability of taking a random action.
+            start_epsilon (float): Initial value of epsilon.
+            end_epsilon (float): Final value of epsilon.
+            decay_steps (int): Number of steps over which epsilon will be decayed from
+                start_epsilon to end_epsilon.
+            decay_schedule (str): Schedule for decaying epsilon, 'const', 'lin', 'exp'.
         """
-        self.cur_epsilon = epsilon
+        self.epsilon_updater = common.ParameterUpdater(
+            start_epsilon, end_epsilon, decay_steps, decay_schedule)
 
     def act(self, q_values, *args, **kwargs):
         """Choose the action from the given Q values, update epsilon, and return the action.
@@ -64,22 +73,12 @@ class EpsilonGreedyPolicy(BasePolicy):
         Returns:
             A 1-dimensional numpy array with the index of the chosen action.
         """
-        rand_action = np.random.choice([True, False], p=[self.cur_epsilon, 1 - self.cur_epsilon])
-        if rand_action:
+        if np.random.binomial(1, p=self.epsilon_updater.cur_value):
             action = np.array([np.random.choice(range(len(q_values)))])
         else:
             action = np.array([np.argmax(q_values)])
-        self._update_params()
+        self.epsilon_updater.update()
         return action
-
-    @abc.abstractmethod
-    def _update_params(self):
-        """Update the policy parameters.
-
-        This is called in every execution of act() and should implement any parameter update that
-        happens at every act() call.
-        """
-        pass
 
 
 class GreedyPolicy(BasePolicy):
@@ -96,93 +95,6 @@ class GreedyPolicy(BasePolicy):
             A 1-dimensional numpy array with the index of the action that maximizes the Q values.
         """
         return np.array([np.argmax(q_values)])
-
-
-class FixedEpsilonGreedyPolicy(EpsilonGreedyPolicy):
-    """An epsilon-greedy policy that takes a random action with p = epsilon, and an argmax action
-    with p = (1 - epsilon). This policy only works for single actions and not for multi-discrete
-    actions.
-    """
-
-    def _update_params(self):
-        """Does nothing, since epsilon is fixed.
-        """
-        pass
-
-
-class LinearDecayEpsilonGreedyPolicy(EpsilonGreedyPolicy):
-    """An epsilon-greedy policy where the epsilon parameter decays linearly with the number of
-    act() calls. Works only with single-action and not for multi-discrete actions.
-    """
-
-    def __init__(self, decay_steps, start_epsilon, end_epsilon):
-        """Instantiate the linear decay epsilon-greedy policy with the linear decay parameters.
-
-        Args:
-            decay_steps (int): Number of act calls necessary to make epsilon decay from
-                start_epsilon to end_epsilon.
-            start_epsilon (float): Initial value of epsilon.
-            end_epsilon (float): Final value of epsilon.
-        """
-        super().__init__(epsilon=start_epsilon)
-        self.decay_steps = decay_steps
-        self.start_epsilon = start_epsilon
-        self.end_epsilon = end_epsilon
-        self.step = 0
-
-    def _update_params(self):
-        """Linearly decay epsilon, silently setting it at zero if it becomes negative.
-        """
-        # Epsilon update
-        self.cur_epsilon = self.cur_epsilon - \
-                           (self.start_epsilon - self.end_epsilon) / self.decay_steps
-        if self.cur_epsilon < 0:
-            self.cur_epsilon = 0
-
-    def reset(self):
-        """Reset the decay parameters to their initial value
-        """
-        self.cur_epsilon = self.start_epsilon
-        self.step = 0
-
-
-class ExponentialDecayEpsilonGreedy(EpsilonGreedyPolicy):
-    """Implementation of an exponential decay epsilon-greedy policy.
-
-    The policy takes a random action with p = epsilon and argmax action over the Q values with
-    p = (1 - epsilon). Epsilon exponentially decays with the number of steps, that correspond to
-    the number of act() calls. The formula is epsilon(t) = start_epsilon * alpha ** t,
-    where alpha is computed such that start_epsilon * alpha ** decay_steps = end_epsilon.
-    """
-
-    def __init__(self, decay_steps, start_epsilon, end_epsilon):
-        """Instantiate the exponential decay epsilon-greedy policy.
-
-        Args:
-            decay_steps (int): Number of steps to make epsilon decay to end_epsilon.
-            start_epsilon (float): Initial value of epsilon.
-            end_epsilon (float): Final value of epsilon.
-        """
-        super().__init__(start_epsilon)
-        self.decay_steps = decay_steps
-        self.start_epsilon = start_epsilon
-        self.end_epsilon = end_epsilon
-
-        if end_epsilon == 0:
-            end_epsilon = 1e-4
-        self.alpha = np.power((end_epsilon / start_epsilon), 1./decay_steps)
-        self.step = 0
-
-    def _update_params(self):
-        """Perform one exponential decay step
-        """
-        self.cur_epsilon *= self.alpha
-
-    def reset(self):
-        """Reset the decay parameters to their initial value
-        """
-        self.cur_epsilon = self.start_epsilon
-        self.step = 0
 
 
 class BaseGaussianPolicy(abc.ABC):
@@ -205,16 +117,28 @@ class BaseGaussianPolicy(abc.ABC):
 
 class BaseEpsilonGaussianPolicy(BaseGaussianPolicy):
     """Base class for a parametrized Gaussian policy that adds noise from a standard distribution
-    with zero mean and variance epsilon.
+    with zero mean and variance epsilon, which is updated over act() calls with the given schedule.
     """
 
-    def __init__(self, epsilon):
+    def __init__(self, start_epsilon, end_epsilon, decay_steps, decay_schedule='const',
+                 min_action=None, max_action=None):
         """Instantiate the Gaussian policy.
 
         Args:
-            epsilon (float): Variance of the Gaussian distribution.
+            start_epsilon (float): Initial variance of the distribution.
+            end_epsilon (float): Final variance of the distribution.
+            decay_steps (int): Number of steps over which epsilon is updated from start_epsilon
+                to end_epsilon.
+            decay_schedule (str): 'const', 'lin', 'exp'.
+            min_action (Union[float, numpy.ndarray]): Low clipping value of sampled actions.
+                Leave None for no clipping.
+            max_action (Union[float, numpy.ndarray]): High clipping value of sampled actions.
+                Leave None for no clipping.
         """
-        self.cur_epsilon = epsilon
+        self.min_action = min_action
+        self.max_action = max_action
+        self.epsilon_updater = common.ParameterUpdater(
+            start_epsilon, end_epsilon, decay_steps, decay_schedule)
 
     def act(self, mean, *args, **kwargs):
         """Compute the action by sampling from a standard Gaussian with zero mean and epsilon
@@ -226,91 +150,8 @@ class BaseEpsilonGaussianPolicy(BaseGaussianPolicy):
         Returns:
             A Tensor with the same shape of mean, containing the chosen action.
         """
-        noise = torch.randn_like(mean) * self.cur_epsilon
+        noise = torch.randn_like(mean) * np.sqrt(self.epsilon_updater.cur_value)
         action = mean + noise
-        self._update_params()
+        action = common.clamp(action, self.min_action, self.max_action)
+        self.epsilon_updater.update()
         return action
-
-    @abc.abstractmethod
-    def _update_params(self):
-        """Update the policy parameters.
-
-        This is called in every execution of act() and should implement any parameter update that
-        happens at every act() call.
-        """
-        pass
-
-
-class FixedEpsilonGaussianPolicy(BaseEpsilonGaussianPolicy):
-    """Implementation of a Gaussian policy that adds to the given action noise from N(0, epsilon)
-    with fixed epsilon.
-    """
-
-    def _update_params(self):
-        """Do nothing here.
-        """
-        pass
-
-
-class LinearDecayEpsilonGaussianPolicy(BaseEpsilonGaussianPolicy):
-    """Implementation of a Gaussian policy that adds to the given action noise from N(0, epsilon)
-    where epsilon is linearly decreased with the number of act() calls.
-    """
-
-    def __init__(self, start_epsilon, end_epsilon, decay_steps):
-        super().__init__(start_epsilon)
-        self.start_epsilon = start_epsilon
-        self.end_epsilon = end_epsilon
-        self.decay_steps = decay_steps
-        self.step = 0
-
-    def reset(self):
-        self.cur_epsilon = self.start_epsilon
-        self.step = 0
-
-    def _update_params(self):
-        """Linearly decay epsilon, silently setting it at zero if it becomes negative.
-                """
-        # Epsilon update
-        self.cur_epsilon = self.cur_epsilon - \
-                           (self.start_epsilon - self.end_epsilon) / self.decay_steps
-        if self.cur_epsilon < 0:
-            self.cur_epsilon = 0
-
-
-class ExponentialDecayEpsilonGaussianPolicy(BaseEpsilonGaussianPolicy):
-    """Implementation of an exponential decay Gaussian policy.
-
-    Epsilon exponentially decays with the number of steps of act() calls.
-    The formula of the decay is epsilon(t) = start_epsilon * alpha ** t,
-    where alpha is computed such that start_epsilon * alpha ** decay_steps = end_epsilon.
-    """
-
-    def __init__(self, decay_steps, start_epsilon, end_epsilon):
-        """Instantiate the exponential decay Gaussian policy.
-
-        Args:
-            decay_steps (int): Number of steps to make epsilon decay to end_epsilon.
-            start_epsilon (float): Initial value of epsilon.
-            end_epsilon (float): Final value of epsilon.
-        """
-        super().__init__(start_epsilon)
-        self.decay_steps = decay_steps
-        self.start_epsilon = start_epsilon
-        self.end_epsilon = end_epsilon
-
-        if end_epsilon == 0:
-            end_epsilon = 1e-4
-        self.alpha = np.power((end_epsilon / start_epsilon), 1. / decay_steps)
-        self.step = 0
-
-    def _update_params(self):
-        """Perform one exponential decay step
-        """
-        self.cur_epsilon *= self.alpha
-
-    def reset(self):
-        """Reset the decay parameters to their initial value
-        """
-        self.cur_epsilon = self.start_epsilon
-        self.step = 0
