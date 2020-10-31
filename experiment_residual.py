5068d069997c658bf4f16a6c87154ae93a53e657
Federico <federico.taschin@studenti.unipd.it>
2020-10-31 16:56:43 +0100

    Bigger networks for residual experiment

--- experiment_residual.py
+++ experiment_residual.py
@@ -8,12 +8,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 import networks
-import algorithms
+import td3
 import experiment_utils
 import hardcoded_policies
 from deepq import replay_buffers
 
-BACKBONE_RESULTS_DIR = 'experiment_results/td3/backbone_experiments/'
 
 NUM_RUNS = 10
 TRAINING_STEPS = 80000
@@ -24,20 +23,21 @@ RL_ACTOR_LR = 0.5e-3
 EPSILON_START = 0.1
 EPSILON_END = 0.1
 EPSILON_DECAY_SCHEDULE = 'const'
+CHECKPOINT_EVERY = 5000
 
 
 def get_actor_critic(state_len, action_len, max_action):
     critic = networks.LinearNetwork(
         inputs=state_len + action_len,
         outputs=1,
-        n_hidden_layers=1,
+        n_hidden_layers=3,
         n_hidden_units=128,
         activation=F.relu
     )
     actor = networks.LinearNetwork(
         inputs=state_len,
         outputs=action_len,
-        n_hidden_layers=1,
+        n_hidden_layers=3,
         n_hidden_units=128,
         activation=F.relu,
         activation_last_layer=torch.tanh,
@@ -46,7 +46,8 @@ def get_actor_critic(state_len, action_len, max_action):
     return actor, critic
 
 
-def train_with_backbone(env: gym.Env, train_steps, num_runs, backbone_policy, exp_name_prefix=''):
+def train_with_backbone(env: gym.Env, train_steps, num_runs, backbone_policy, exp_name_prefix='',
+                        results_dir):
     """This experiment pre-trains the actor network (and optionally the critic), then runs the TD3
     algorithm using the pre-trained actor (critic).
 
@@ -58,7 +59,7 @@ def train_with_backbone(env: gym.Env, train_steps, num_runs, backbone_policy, ex
         exp_name_prefix (str): Prefix to be added to experiment name.
     """
     exp_name = exp_name_prefix + backbone_policy.__name__
-    if os.path.exists(os.path.join(BACKBONE_RESULTS_DIR, exp_name + '.npy')):
+    if os.path.exists(os.path.join(results_dir, exp_name + '.npy')):
         warn = 'Warning: ' + str(exp_name) + ' already exists. Skipping.'
         print(warn)
         return warn
@@ -76,17 +77,18 @@ def train_with_backbone(env: gym.Env, train_steps, num_runs, backbone_policy, ex
         actor, critic = get_actor_critic(state_len, action_len, max_action)
 
         # Train
-        td3 = algorithms.TD3(critic, actor, training_steps=train_steps, max_action=max_action,
-                             min_action=-max_action, critic_lr=RL_CRITIC_LR,
-                             actor_lr=RL_CRITIC_LR, evaluate_every=-1,
-                             backbone_policy=backbone_policy,
-                             epsilon_start=EPSILON_START, epsilon_end=EPSILON_END,
-                             epsilon_decay_schedule=EPSILON_DECAY_SCHEDULE)
+        td3_algorithm = td3.TD3(critic, actor, training_steps=train_steps, max_action=max_action,
+                                min_action=-max_action, critic_lr=RL_CRITIC_LR,
+                                actor_lr=RL_CRITIC_LR, evaluate_every=-1,
+                                backbone_policy=backbone_policy,
+                                epsilon_start=EPSILON_START, epsilon_end=EPSILON_END,
+                                epsilon_decay_schedule=EPSILON_DECAY_SCHEDULE,
+                                checkpoint_every=CHECKPOINT_EVERY)
         prefiller = replay_buffers.BufferPrefiller(
             num_transitions=BUFFER_PREFILL_STEPS, collection_policy=backbone_policy,
             collection_policy_noise=COLLECTION_POLICY_NOISE, min_action=-max_action,
             max_action=max_action, use_residual=True)
-        train_res = td3.train(env, buffer_prefiller=prefiller)
+        train_res = td3_algorithm.train(env, buffer_prefiller=prefiller)
 
         train_scores.append(experiment_utils.Plot(
             train_res['end_steps'], train_res['rewards'], name='train'))
@@ -99,10 +101,10 @@ def train_with_backbone(env: gym.Env, train_steps, num_runs, backbone_policy, ex
         'train': train_scores
     }
 
-    experiment_utils.save_results_numpy(BACKBONE_RESULTS_DIR, exp_name, data_dict)
+    experiment_utils.save_results_numpy(results_dir, exp_name, data_dict)
 
 
-def standard_training(env, train_steps, num_runs):
+def standard_training(env, train_steps, num_runs, results_dir):
     """Perform standard training with TD3. and saves the results.
 
     Args:
@@ -119,13 +121,15 @@ def standard_training(env, train_steps, num_runs):
     for run in range(num_runs):
         actor, critic = get_actor_critic(state_len, action_len, max_action)
 
-        td3 = algorithms.TD3(critic, actor, training_steps=train_steps, max_action=max_action,
-                             min_action=-max_action, critic_lr=RL_CRITIC_LR,
-                             actor_lr=RL_CRITIC_LR, evaluate_every=-1,
-                             epsilon_decay_schedule=EPSILON_DECAY_SCHEDULE)
-        prefiller = replay_buffers.BufferPrefiller(
-            num_transitions=BUFFER_PREFILL_STEPS)
-        train_res = td3.train(env, buffer_prefiller=prefiller)
+        # Train
+        td3_algorithm = td3.TD3(critic, actor, training_steps=train_steps, max_action=max_action,
+                                min_action=-max_action, critic_lr=RL_CRITIC_LR,
+                                actor_lr=RL_CRITIC_LR, evaluate_every=-1,
+                                epsilon_start=EPSILON_START, epsilon_end=EPSILON_END,
+                                epsilon_decay_schedule=EPSILON_DECAY_SCHEDULE,
+                                checkpoint_every=CHECKPOINT_EVERY)
+        prefiller = replay_buffers.BufferPrefiller(num_transitions=BUFFER_PREFILL_STEPS)
+        train_res = td3_algorithm.train(env, buffer_prefiller=prefiller)
         train_scores.append(experiment_utils.Plot(
             train_res['end_steps'], train_res['rewards'], name='train'))
         train_eval_scores.append(experiment_utils.Plot(
@@ -138,64 +142,20 @@ def standard_training(env, train_steps, num_runs):
         'train': train_scores
     }
     exp_name = standard_training.__name__
-    experiment_utils.save_results_numpy(BACKBONE_RESULTS_DIR, exp_name, data_dict)
-
-
-def plot_experiment_1(dir, exp_results_filename, standard_training_filename, offset=True):
-    exp_res = experiment_utils.read_result_numpy(dir, exp_results_filename)
-    standard_train_res = experiment_utils.read_result_numpy(dir, standard_training_filename)
-    x_pretrain, y_pretrain, _, _ = experiment_utils.merge_plots(exp_res['pretrain'])
-    x_train, y_train, _, _ = experiment_utils.merge_plots(exp_res['train'])
-    x_eval, y_eval, _, _, = experiment_utils.merge_plots(exp_res['eval'])
-    if offset:
-        x_train += x_pretrain[-1]
-        x_eval += x_pretrain[-1]
-
-    x_standard_train, y_standard_train, _, _ = experiment_utils.merge_plots(
-        standard_train_res['train'])
-    print(y_standard_train)
-    x_standard_eval, y_standadrd_eval, _, _ = experiment_utils.merge_plots(
-        standard_train_res['eval'])
-
-    plt.plot(x_pretrain, y_pretrain, label='Pretraining evaluation scores')
-    plt.plot(x_train, y_train, label='Training rewards')
-    plt.plot(x_eval, y_eval, label='Training evaluation scores')
-    plt.plot(x_standard_train, y_standard_train, label='Standard training rewards')
-    plt.plot(x_standard_train, y_standard_train, label='Standard training evaluations')
-    plt.legend()
-    plt.show()
-
-
-def plot(dir, max_in_plot=2, always_plot='standard_training.npy', cut_pretrain_at=150,
-         only_files=None):
-    if only_files is None:
-        files = os.listdir(dir)
-        files.remove(always_plot)
-    else:
-        files = only_files
-    for i, file in enumerate(files):
-        res = experiment_utils.read_result_numpy(dir, file)
-        x, y, _, _ = experiment_utils.merge_plots(res['train'])
-        if 'pretrain' in res:
-            x_pretrain, y_pretrain, _, _ = experiment_utils.merge_plots(res['pretrain'])
-            x_pretrain = x_pretrain[:cut_pretrain_at]
-            y_pretrain = y_pretrain[:cut_pretrain_at]
-            x += x_pretrain[-1]
-            plt.plot(x_pretrain, y_pretrain, 'k')
-        plt.plot(x, y, label=file)
-        if (i + 1) % max_in_plot == 0 and i > 0 or i == len(files) - 1:
-            res = experiment_utils.read_result_numpy(
-                experiment_utils.PENDULUM_TD3_RESULTS_DIR, always_plot)
-            x_alw, y_alw, _, _ = experiment_utils.merge_plots(res['train'])
-            plt.plot(x_alw, y_alw, label=always_plot)
-            plt.legend()
-            plt.show()
+    experiment_utils.save_results_numpy(results_dir, exp_name, data_dict)
 
 
 if __name__ == '__main__':
-    env = gym.make('LunarLanderContinuous-v2')
-    _actor = torch.load('models/LunarLanderContinuous-v2/actor_80000')
+    _env = gym.make('LunarLanderContinuous-v2')
+    _results_dir = 'experiment_results/td3/lunar_lander'
 
+    # Perform standard training
+    standard_training(_env, train_steps=TRAINING_STEPS, num_runs=10, results_dir=_results_dir)
+
+    """
+    # Perform training with backbone policy
+    _actor = torch.load('models/LunarLanderContinuous-v2/actor_80000')
+    
     def model_policy(state):
         with torch.no_grad():
             action = _actor(
@@ -203,22 +163,8 @@ if __name__ == '__main__':
             )[0].detach().numpy()
         return action
 
-    standard_training(env, train_steps=TRAINING_STEPS, num_runs=10)
     train_with_backbone(
         exp_name_prefix='actor_80000', env=env, train_steps=TRAINING_STEPS, num_runs=10,
         backbone_policy=model_policy
     )
-
-    exit()
-    standard = experiment_utils.read_result_numpy(experiment_utils.PENDULUM_TD3_RESULTS_DIR,
-                                                  'standard_training.npy')
-    std_x, std_y, _, _ = experiment_utils.merge_plots(standard['train'])
-
-    backbone = experiment_utils.read_result_numpy(BACKBONE_RESULTS_DIR, 'backbone_policy.npy')
-    backbone_x, backbone_y, _, _ = experiment_utils.merge_plots(backbone['train'])
-
-    plt.plot(std_x, std_y, label='standard')
-    plt.plot(backbone_x, backbone_y, label='backbone')
-    plt.legend()
-    plt.show()
-
+    """
